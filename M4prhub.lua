0045ea0745990f437117fc08328cb89f45ea74fd
UselessMaper <jumpking00100@gmail.com>
2026-08-07 10:00:23 +0700

    Update window name to 'M4pr Hub'

--- M4prhub.lua
+++ M4prhub.lua
@@ -1,7 +1,7 @@
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 
 local Window = Rayfield:CreateWindow({
-    Name = "M4pr Hub v1.3",
+    Name = "M4pr Hub",
     LoadingTitle = "Thanks for using! :3",
     LoadingSubtitle = "by Useless Maper"
 })
