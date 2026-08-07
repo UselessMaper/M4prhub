e3a833c54509a82931dc14123fad0bac4285438a
UselessMaper <jumpking00100@gmail.com>
2026-08-08 01:46:04 +0700

    Change DashstabStuds value from 3 to 2

--- M4prhub.lua
+++ M4prhub.lua
@@ -141,7 +141,7 @@ local BackstabEnabled = false
 local BackstabStuds = 2
 
 local DashstabEnabled = false
-local DashstabStuds = 3
+local DashstabStuds = 2
 local DashstabDelay = 0
 local DashstabDuration = 1
 local TweenTime = 0.3
@@ -425,7 +425,7 @@ TwoTimeTab:CreateSlider({
     Range = {0, 5},
     Increment = 1,
     Suffix = "studs",
-    CurrentValue = 3,
+    CurrentValue = 2,
     Flag = "DashstabStuds",
 
     Callback = function(Value)
