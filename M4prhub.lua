9543f111b5d69ac914c35d167fdd9325947ebaa0
UselessMaper <jumpking00100@gmail.com>
2026-08-09 00:53:32 +0700

    Update label text for backstab and dashstab

--- M4prhub.lua
+++ M4prhub.lua
@@ -377,7 +377,7 @@ TwoTimeTab:CreateSlider({
     end,
 })
 
-TwoTimeTab:CreateLabel("You should use 2-3 studs")
+TwoTimeTab:CreateLabel("It's better 2-4 studs for backstab and dashstab")
 
 -- =========================
 -- DASHSTAB TOGGLE
