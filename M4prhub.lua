4e6c3b1a9a11c3d2491382480b6bbbf50557b57d
UselessMaper <jumpking00100@gmail.com>
2026-09-20 10:14:50 +0700

    Update SCRIPT_KEY in M4prhub.lua

--- M4prhub.lua
+++ M4prhub.lua
@@ -1,2 +1,2 @@
-getgenv().SCRIPT_KEY = "cc2ccbe9-6076-440b-82db-cdee74ee09aa"
+getgenv().SCRIPT_KEY = "93e8d35b-39b1-4793-a818-ad3de6f1453b"
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/b1037d290480413a9de457a218478f3fa86168abba5e92540919ab8629423488/download"))()
